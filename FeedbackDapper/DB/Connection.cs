cb156d07f08671d00175a5e225db0e43697c4946
Avinash Jorige <jo.avi.1990@gmail.com>
2019-07-17 23:15:38 +0530

    Initial changes

---
+++ FeedbackDapper/DB/Connection.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Web.Configuration;
+
+namespace FeedbackDapper.DB
+{
+    public class Connection
+    {
+        private readonly Configuration config;
+        private readonly ConnectionStringsSection sec;
+        public Connection()
+        {
+            this.config = WebConfigurationManager.OpenWebConfiguration("~");
+            sec = (ConnectionStringsSection)config.GetSection("connectionStrings");
+            sec.ConnectionStrings["myConnectionString"].ConnectionString = "Data Source=DESKTOP-13LL2HE\\SQLSERVER;Initial Catalog=Practice;Persist Security Info=True;User ID=sa;Password=jorige";
+            config.Save();
+        }
+    }
+}
---
+++ FeedbackDapper/Utility/Helper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace FeedbackDapper.Utility
+{
+    public static class Helper
+    {
+        private static readonly string strFileName = "DBQueries";
+        public static string GetQuery(string strQueryName, string strQueryId)
+        {
+            var path = HttpContext.Current.Server.MapPath("~/Config");
+            try
+            {
+                if (!string.IsNullOrEmpty(path.ToString()) && Directory.Exists(path))
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return string.Empty;
+        }
+    }
+}
